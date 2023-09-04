80220964b31959aa4580a7eb1e9aa5e3f5942639
SolForte <mateusbrandao1234@gmail.com>
2023-09-04 12:31:34 -0300

    build: prisma database creation migration

---
+++ prisma/migrations/20230904152954_database_creation/migration.sql
@@ -0,0 +1,78 @@
+-- CreateEnum
+CREATE TYPE "CardType" AS ENUM ('CREDIT', 'DEBIT', 'BOTH');
+
+-- CreateTable
+CREATE TABLE "users" (
+    "id" SERIAL NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "credentials" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "url" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "authorId" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "credentials_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "annotations" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "authorId" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "annotations_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "creditCards" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "number" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "expirationDate" TIMESTAMP(3) NOT NULL,
+    "secureCode" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "isVirtual" BOOLEAN NOT NULL,
+    "type" "CardType" NOT NULL,
+    "authorId" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "creditCards_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "credentials_authorId_title_key" ON "credentials"("authorId", "title");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "annotations_authorId_title_key" ON "annotations"("authorId", "title");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "creditCards_authorId_title_key" ON "creditCards"("authorId", "title");
+
+-- AddForeignKey
+ALTER TABLE "credentials" ADD CONSTRAINT "credentials_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "annotations" ADD CONSTRAINT "annotations_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "creditCards" ADD CONSTRAINT "creditCards_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
